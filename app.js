284f452d6a9d7bbf34580aa3d5e067085b9c4300
vikingviolinist <misopetrik@gmail.com>
2018-04-14 09:50:10 +0200

    Adds jquery library, dives deep into in & Learns polyfill and inheritance

--- app.js
+++ app.js
@@ -36,3 +36,46 @@ var jim = {
 _.extend(john, jane, jim);
 
 console.log(john);
+
+
+// Polyfill
+if(!Object.create) {
+    Object.create = function(o) {
+        if (arguments.length  >1) {
+            throw new Error('Object.create implementation'
+            + ' only accepts the first parameter.');
+        }
+        function F() {}
+        F.prototype = o;
+        return new F();
+    }
+}
+
+// Object.create & pure prototypal inheritance
+var person = {
+    firstname: 'Default',
+    lastname: 'Default',
+    greet: function() {
+        return 'Hi ' + this.firstname;
+    }
+}
+
+var john = Object.create(person);
+john.firstname = 'John';
+john.lastname = 'Doe';
+console.log(john);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
